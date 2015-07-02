9978abe7d1a39330ba8879cfb1680bbaca801de8
Jason Holmes <holmes@squareup.com>
2015-07-02 08:26:25 -0700

    Initial Commit

---
+++ app/src/test/java/com/holmes/sample/SimpleTest.java
@@ -0,0 +1,12 @@
+package com.holmes.sample;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+public class SimpleTest {
+
+  @Test public void testSomething() {
+    assertTrue(true);
+  }
+}
